f61c049d71f829c3e90d1266c9ebaffa8863f5b8
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-10-03 23:10:42 +0200

    Moved more functionality to the visitor class.

--- jsonformat/jsonformat.py
+++ jsonformat/jsonformat.py
@@ -1,11 +1,9 @@
 
 
 class JSONNode:
-    def __init__(self, indent=2):
+    def __init__(self, name, indent=2):
+        self.name = name
         self._indent = indent
-
-    def _indent_str(self):
-        return "".ljust(self._indent)
 
     def dump(self, data):
         builder = JSONBuilder()
@@ -18,43 +16,18 @@ class JSONNode:
 
 class JSONString(JSONNode):
     def __init__(self, name=None, indent=2):
-        JSONNode.__init__(self, indent)
+        JSONNode.__init__(self, name, indent)
         self.__name = name
 
 
 class JSONContainer(JSONNode):
-    def __init__(self, indent=2):
-        JSONNode.__init__(self, indent)
+    def __init__(self, name=None, indent=2):
+        JSONNode.__init__(self, name, indent)
         self._items = []
-
-    def _inc_indent_level(self):
-        return self._indent
-
-    def _start(self, name, end):
-        if name:
-            return "\"" + name + "\": " + end
-        else:
-            return end
-
-    def _end(self, end):
-        return end
 
     def accept_items(self, visitor):
         for item in self._items:
             item.accept(visitor)
-
-    def _get_items(self, data):
-        temp = []
-        result = []
-        for item in self._items:
-            temp.append(item.dump(data))
-
-        if len(temp) > 0:
-            result.append(",\n".join(temp))
-        else:
-            # Handle no items.
-            result.append("\n")
-        return result
 
     def add_string(self, name):
         string = JSONString(name)
@@ -74,8 +47,7 @@ class JSONContainer(JSONNode):
 
 class JSONArray(JSONContainer):
     def __init__(self, name=None, indent=2):
-        JSONContainer.__init__(self, indent)
-        self.name = name
+        JSONContainer.__init__(self, name, indent)
 
     def add_array(self):
         return self._add_array(None)
@@ -84,20 +56,15 @@ class JSONArray(JSONContainer):
         return self._add_dictionary(None)
 
     def accept(self, visitor):
-        visitor.visit_array_start(self)
-        self.accept_items(visitor)
-        visitor.visit_array_end(self)
+        visitor.visit_array(self)
 
 
 class JSONDictionary(JSONContainer):
     def __init__(self, name=None, indent=2):
-        JSONContainer.__init__(self, indent)
-        self.name = name
+        JSONContainer.__init__(self, name, indent)
 
     def accept(self, visitor):
-        visitor.visit_dictionary_start(self)
-        self.accept_items(visitor)
-        visitor.visit_dictionary_end(self)
+        visitor.visit_dictionary(self)
 
     def add_array(self, name):
         return self._add_array(name)
@@ -106,30 +73,47 @@ class JSONDictionary(JSONContainer):
         return self._add_dictionary(name)
 
 
+class IndentationLevel:
+    def __init__(self):
+        self.__level = 0
+
+    def inc(self, level=2):
+        self.__level += level
+
+    def dec(self, level=2):
+        self.__level -= level
+
+    def __str__(self):
+        return "".ljust(self.__level)
+
+
 class JSONBuilder:
     def __init__(self):
         self.__result = []
+        self.__indentation = IndentationLevel()
 
-    def visit_dictionary_start(self, dictionary):
+    def visit_dictionary(self, dictionary):
         if dictionary.name is not None:
-            self.__result.append(f"{dictionary.name}: {{")
+            self.__result.append(f"{self.__indentation}{dictionary.name}: {{")
         else:
-            self.__result.append(f"{{")
+            self.__result.append(f"{self.__indentation}{{")
+        self.__indentation.inc()
+        dictionary.accept_items(self)
+        self.__indentation.dec()
+        self.__result.append(f"{self.__indentation}}}")
 
-    def visit_dictionary_end(self, dictionary):
-        self.__result.append("}")
-
-    def visit_array_start(self, array):
+    def visit_array(self, array):
         if array.name is not None:
-            self.__result.append(f"{array.name}: [")
+            self.__result.append(f"{self.__indentation}{array.name}: [")
         else:
-            self.__result.append(f"[")
-
-    def visit_array_end(self, array):
-        self.__result.append(f"]")
+            self.__result.append(f"{self.__indentation}[")
+        self.__indentation.inc()
+        array.accept_items(self)
+        self.__indentation.dec()
+        self.__result.append(f"{self.__indentation}]")
 
     def get_result(self):
         return "\n".join(self.__result)
 
     def visit_node(self, node):
-        self.__result.append("<node>")
+        self.__result.append(f"{self.__indentation}{node.name}")
