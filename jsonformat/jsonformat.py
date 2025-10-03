b751ee9ce66556685a726b466f37191117dd68ab
Peter Hallonmark <peter.hallonmark@gmail.com>
2025-10-03 17:41:03 +0200

    Restructured the code to reasamble the visitor pattern instead.

--- jsonformat/jsonformat.py
+++ jsonformat/jsonformat.py
@@ -8,17 +8,18 @@ class JSONNode:
         return "".ljust(self._indent)
 
     def dump(self, data):
-        return []
+        builder = JSONBuilder()
+        self.accept(builder)
+        return builder.get_result()
+        
+    def accept(self, visitor):
+        visitor.visit_node(self)
 
 
 class JSONString(JSONNode):
     def __init__(self, name=None, indent=2):
         JSONNode.__init__(self, indent)
         self.__name = name
-
-    def dump(self, data):
-        result = "\"" + self.__name + "\": \"{value}\""
-        return result
 
 
 class JSONContainer(JSONNode):
@@ -37,6 +38,10 @@ class JSONContainer(JSONNode):
 
     def _end(self, end):
         return end
+
+    def accept_items(self, visitor):
+        for item in self._items:
+            item.accept(visitor)
 
     def _get_items(self, data):
         temp = []
@@ -70,22 +75,7 @@ class JSONContainer(JSONNode):
 class JSONArray(JSONContainer):
     def __init__(self, name=None, indent=2):
         JSONContainer.__init__(self, indent)
-        self.__name = name
-
-    def dump(self, data):
-        result = [self._start(self.__name, "[")]
-        temp = []
-        for item in self._items:
-            temp.append(item.dump(data))
-
-        if len(temp) > 0:
-            result.append(",\n".join(temp))
-        else:
-            # Handle no items.
-            result.append("\n")
-
-        result.append(self._end("]"))
-        return self._indent_str() + self._indent_str().join(result)
+        self.name = name
 
     def add_array(self):
         return self._add_array(None)
@@ -93,26 +83,21 @@ class JSONArray(JSONContainer):
     def add_dictionary(self):
         return self._add_dictionary(None)
 
+    def accept(self, visitor):
+        visitor.visit_array_start(self)
+        self.accept_items(visitor)
+        visitor.visit_array_end(self)
+
 
 class JSONDictionary(JSONContainer):
     def __init__(self, name=None, indent=2):
         JSONContainer.__init__(self, indent)
-        self.__name = name
+        self.name = name
 
-    def _build(self, builder):
-        builder.start_section(self.__name, "{")
-
-        builder.end_section("}")
-
-    def dump(self, data):
-        result = [self._start(self.__name, "{")]
-        builder = JSONBuilder()
-        self._build(builder)
-        #print(self.indent_level_str() + str(self.__name) + " " + str(len(self._items)))
-        #print(self._get_items(data))
-        #result.append(self._get_items(data))
-        result.append(self._end("}"))
-        return "\n".join(result)
+    def accept(self, visitor):
+        visitor.visit_dictionary_start(self)
+        self.accept_items(visitor)
+        visitor.visit_dictionary_end(self)
 
     def add_array(self, name):
         return self._add_array(name)
@@ -123,10 +108,28 @@ class JSONDictionary(JSONContainer):
 
 class JSONBuilder:
     def __init__(self):
-        pass
+        self.__result = []
 
-    def start_section(self, name, aa):
-        print("".ljust(2) + str(name) + aa)
+    def visit_dictionary_start(self, dictionary):
+        if dictionary.name is not None:
+            self.__result.append(f"{dictionary.name}: {{")
+        else:
+            self.__result.append(f"{{")
 
-    def end_section(self, aa):
-        print("".ljust(2) + aa)
+    def visit_dictionary_end(self, dictionary):
+        self.__result.append("}")
+
+    def visit_array_start(self, array):
+        if array.name is not None:
+            self.__result.append(f"{array.name}: [")
+        else:
+            self.__result.append(f"[")
+
+    def visit_array_end(self, array):
+        self.__result.append(f"]")
+
+    def get_result(self):
+        return "\n".join(self.__result)
+
+    def visit_node(self, node):
+        self.__result.append("<node>")
